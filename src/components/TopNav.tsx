961aa5f6dfd7fcbb8c3e8198aca03e61e2b9d6c3
Romeo <romeomeshack34@gmail.com>
2026-06-01 12:04:00 +0100

    Modified

--- src/components/TopNav.tsx
+++ src/components/TopNav.tsx
@@ -72,7 +72,7 @@ export default function TopNav() {
           </Link>
 
           <Link
-            to="/work"
+            to="/our-work"
             className="flex items-center gap-2 text-2xl font-medium hover:text-duo-green-200 transition-colors"
             onClick={() => setIsMenuOpen(false)}
           >
